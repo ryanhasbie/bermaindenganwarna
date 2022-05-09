eabc2f0f8d81de67adcf6e76724fc352a7010639
ryanhasbie <ryanhasbie2001@gmail.com>
2022-05-10 06:27:26 +0700

    menambahkan eventlistener slider merah

--- script.js
+++ script.js
@@ -18,3 +18,7 @@ acakWarna.addEventListener('click', function () {
 });
 
 const sMerah = document.querySelector('input[name=sMerah]');
+sMerah.addEventListener('input', function () {
+    const r = sMerah.value;
+    document.body.style.backgroundColor = 'rgb(' + r + ',100,100)';
+});
