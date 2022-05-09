347da7c347a8e5f3f4ff246443db9af31d105ad6
ryanhasbie <ryanhasbie2001@gmail.com>
2022-05-10 05:38:39 +0700

    menambahkan event acak warna

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ const tAcakWarna = document.createTextNode('Acak Warna');
 acakWarna.appendChild(tAcakWarna);
 acakWarna.setAttribute('type', 'submit');
 ubahWarna.after(acakWarna);
+
+acakWarna.addEventListener('click', function () {
+    const r = Math.round(Math.random() * 255 + 1);
+    const g = Math.round(Math.random() * 255 + 1);
+    const b = Math.round(Math.random() * 255 + 1);
+    document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+});
