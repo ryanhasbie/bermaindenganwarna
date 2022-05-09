815cdc3ab9528f0c02a0c8f5a8bf0a6ac96eb144
ryanhasbie <ryanhasbie2001@gmail.com>
2022-05-10 05:28:05 +0700

    membuat button baru dengan js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const ubahWarna = document.getElementById('tUbahWarna');
 ubahWarna.onclick = function () {
-    document.body.clastList.toggle = 'light-green';
-}
+    document.body.classList.toggle('light-green');
+};
+
+
+const acakWarna = document.createElement('button');
+const tAcakWarna = document.createTextNode('Acak Warna');
+acakWarna.appendChild(tAcakWarna);
+acakWarna.setAttribute('type', 'submit');
+ubahWarna.after(acakWarna);
