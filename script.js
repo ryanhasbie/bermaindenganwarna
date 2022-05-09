a9f53021368cad6f6ef464e7a5124326f0350d1f
ryanhasbie <ryanhasbie2001@gmail.com>
2022-05-10 06:35:01 +0700

    mendengarkan event warna biru

--- script.js
+++ script.js
@@ -19,16 +19,26 @@ acakWarna.addEventListener('click', function () {
 
 const sMerah = document.querySelector('input[name=sMerah]');
 const sHijau = document.querySelector('input[name=sHijau]');
+const sBiru = document.querySelector('input[name=sBiru]');
 
 
 sMerah.addEventListener('input', function () {
     const r = sMerah.value;
     const g = sHijau.value;
-    document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',100)';
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
 });
 
 sHijau.addEventListener('input', function () {
     const r = sMerah.value;
     const g = sHijau.value;
-    document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',100)';
-})
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+});
+
+sBiru.addEventListener('input', function () {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+});
